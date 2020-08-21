88f9f2a43a9a1208ce35ebc914c5c49dc784b52d
raghukm2011 <raghukannan786@gmail.com>
2020-08-21 22:52:22 +0530

    Updates

---
+++ precache-manifest.30a8b45a74f376b8168ae25ce6faa428.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e1a6653a10277aeca54a7c1b6719e6",
+    "url": "/simba-deploy/index.html"
+  },
+  {
+    "revision": "225b27d779422faf47f2",
+    "url": "/simba-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "543acca4c8bce84d8790",
+    "url": "/simba-deploy/static/js/2.b4d68586.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simba-deploy/static/js/2.b4d68586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225b27d779422faf47f2",
+    "url": "/simba-deploy/static/js/main.6fd9a502.chunk.js"
+  },
+  {
+    "revision": "21d78a6d9d088397645f",
+    "url": "/simba-deploy/static/js/runtime-main.20175fca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simba-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
